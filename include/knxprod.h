14e5ecde5bdb50b7e6efa2e6dd6347b87f9e7243
Michael Geramb <lasso_riefen0m@icloud.com>
2026-03-02 19:50:41 +0100

    Fix crash in 2nd programming Fix LED brightness

--- include/knxprod.h
+++ include/knxprod.h
@@ -20,7 +20,7 @@
 #define MAIN_OpenKnxId 0xAE
 #define MAIN_ApplicationNumber 44
 #define MAIN_ApplicationVersion 7
-#define MAIN_FirmwareRevision 0
+#define MAIN_FirmwareRevision 1
 #define MAIN_ApplicationEncoding iso-8859-15
 #define MAIN_ParameterSize 8618
 #define MAIN_MaxKoNumber 1059
